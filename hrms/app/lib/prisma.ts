7d94079b9db7fa556ce8af085158d3511463f1f1
monil <kansagaramonil@gmail.com>
2026-01-03 09:44:08 +0530

    project structure

---
+++ hrms/app/lib/prisma.ts
@@ -0,0 +1,15 @@
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "../generated/prisma/client";
+
+const adapter = new PrismaMariaDb({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  connectionLimit: 5,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
